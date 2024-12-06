5ecb1bca96f558139a273df2d1eb5bd11aa6c44e
Saksham Gupta <sakshamprem2003@gmail.com>
2024-12-07 01:49:18 +0530

    cors added

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ require("dotenv").config();
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
-const port = process.env.PORT || 5000;
+const port = 5000;
+const cors = require("cors");
 const dbConfig = require("./config/dbConfig");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
